21a04fd0780bd9fe3dbaa8e84bca1432e6c72582
arslanadeel <arslanadeel@gmail.com>
2020-04-08 05:47:48 +0200

    This contain instruction to del and merge branch

---
+++ del_merge_branch.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# you can delete a branch by
+# git branch -d <branch_name>
+
+#git branch
+
+# this will let you know if some branch is not merged to the
+# master branch
+
+# you merge branch by
+
+# git branch -D <branch_name>
+
+##write you code here
