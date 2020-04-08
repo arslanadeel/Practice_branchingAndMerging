d986acb42b96bad2d9ae1ed8b5bc75e5704f08e2
arslanadeel <arslanadeel@gmail.com>
2020-04-08 05:28:40 +0200

    This contain instruction to creat new branch

---
+++ new-feature.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#You can create a new branch by
+# git checkout <branch_name> 'new-feature'
+#after creating you can switch to this branch by
+# git branch <brach_name>
+#you can check status of branch by
+# git branch
+#current branch will be marked with astaric
+
+#write your code here
